91cf626a63ed8df4611c6e5fd75ea021c83bf121
Sola <dev@sola.love>
2020-10-30 22:26:01 +0800

    support 2nd gen DJMax cards

--- scripts/DJMax.kts
+++ scripts/DJMax.kts
@@ -8,13 +8,14 @@ import java.util.*
 /**
  * This script provides following functions:
  *  - create a new DJMAX Techinka 3 card
- *  - lookup DJMAX Technika 3 card's serial number
+ *  - lookup DJMAX Technika 2/3 card's serial number
  */
 
 val serialChars = ('A'..'Z' union '1'..'9').toList()
 val random = Random()
 
-val keyA = Key("3721536A7240")
+val version2KeyA = Key("23576E6A3021")
+val version3KeyA = Key("3721536A7240")
 val trailerBlock = Block("3721536A7240FF078069FFFFFFFFFFFF")
 while (true) {
     val card = getCard()!!
@@ -29,7 +30,12 @@ while (true) {
         card.writeBlock(2, data2)
         card.writeBlock(3, trailerBlock)
         println("Done.")
-    } else if (card.authBlock(keyA, 0, KeyType.A)) {
+    } else if (card.authBlock(version2KeyA, 0, KeyType.A)) {
+        val data1 = card.readBlock(1)
+        val data2 = card.readBlock(2)
+        val serial = (data1.data() + data2.data()).toString(Charsets.US_ASCII).take(20)
+        println("Version 2 card, Serial: $serial")
+    } else if (card.authBlock(version3KeyA, 0, KeyType.A)) {
         val data1 = card.readBlock(1)
         val data2 = card.readBlock(2)
         val serial = (data1.data() + data2.data()).toString(Charsets.US_ASCII).take(20)
