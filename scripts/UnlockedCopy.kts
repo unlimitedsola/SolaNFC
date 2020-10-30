c57b90e728c3dad13f9700fd0a9a8d543066c17a
Sola <dev@sola.love>
2020-10-30 22:26:34 +0800

    update unlocked copy

--- scripts/UnlockedCopy.kts
+++ scripts/UnlockedCopy.kts
@@ -1,12 +1,22 @@
 import love.sola.nfc.api.getCard
 import love.sola.nfc.api.mifare.classic.functions.unlockedDump
+import love.sola.nfc.api.mifare.classic.functions.unlockedFormat
+import love.sola.nfc.api.waitDisconnect
 import love.sola.nfc.util.toHexString
 
 /**
  * @author Sola
  */
-val card = getCard()!!
+var card = getCard()!!
 println("uid = ${card.getUID().toHexString()}")
 card.unlock()
 val dump = card.unlockedDump()
 println("dump = $dump")
+waitDisconnect()
+
+println("insert target")
+card = getCard()!!
+card.unlock()
+card.unlockedFormat(dump)
+println("disconnect target")
+waitDisconnect()
