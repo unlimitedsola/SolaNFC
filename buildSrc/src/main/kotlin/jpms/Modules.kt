823adbfee5cecfffb532a3957f2663417c274214
Sola <dev@sola.love>
2024-05-26 02:40:48 +0800

    Update modules definition

--- buildSrc/src/main/kotlin/jpms/Modules.kt
+++ buildSrc/src/main/kotlin/jpms/Modules.kt
@@ -2,99 +2,46 @@ package jpms
 
 import org.gradlex.javamodule.moduleinfo.ExtraJavaModuleInfoPluginExtension
 
-/* Dependency Graph
-+--- org.jetbrains.kotlin:kotlin-scripting-common:1.8.21
-|    \--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21
-|         +--- org.jetbrains.kotlin:kotlin-stdlib-common:1.8.21
-|         \--- org.jetbrains:annotations:13.0
-+--- org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.21
-|    +--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-|    \--- org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.21
-|         \--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-+--- org.jetbrains.kotlin:kotlin-reflect:1.8.21
-|    \--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-+--- org.jetbrains.kotlin:kotlin-scripting-jvm:1.8.21
-|    +--- org.jetbrains.kotlin:kotlin-script-runtime:1.8.21
-|    +--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-|    \--- org.jetbrains.kotlin:kotlin-scripting-common:1.8.21 (*)
-\--- org.jetbrains.kotlin:kotlin-scripting-jvm-host:1.8.21
-     +--- org.jetbrains.kotlin:kotlin-script-runtime:1.8.21
-     +--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-     +--- org.jetbrains.kotlin:kotlin-scripting-common:1.8.21 (*)
-     +--- org.jetbrains.kotlin:kotlin-scripting-jvm:1.8.21 (*)
-     +--- org.jetbrains.kotlin:kotlin-compiler-embeddable:1.8.21
-     |    +--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-     |    +--- org.jetbrains.kotlin:kotlin-script-runtime:1.8.21
-     |    +--- org.jetbrains.kotlin:kotlin-reflect:1.6.10 -> 1.8.21 (*)
-     |    +--- org.jetbrains.kotlin:kotlin-daemon-embeddable:1.8.21
-     |    +--- org.jetbrains.intellij.deps:trove4j:1.0.20200330
-     |    \--- net.java.dev.jna:jna:5.6.0
-     \--- org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:1.8.21
-          +--- org.jetbrains.kotlin:kotlin-scripting-compiler-impl-embeddable:1.8.21
-          |    +--- org.jetbrains.kotlin:kotlin-scripting-common:1.8.21 (*)
-          |    +--- org.jetbrains.kotlin:kotlin-scripting-jvm:1.8.21 (*)
-          |    \--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-          \--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
- */
+// Dependency Graph (with duplicates removed):
+// +--- org.jetbrains.kotlin:kotlin-scripting-common:2.0.0
+// |    \--- org.jetbrains.kotlin:kotlin-stdlib:2.0.0
+// |         \--- org.jetbrains:annotations:13.0
+// +--- org.jetbrains.kotlin:kotlin-reflect:2.0.0
+// +--- org.jetbrains.kotlin:kotlin-scripting-jvm:2.0.0
+// |    +--- org.jetbrains.kotlin:kotlin-script-runtime:2.0.0
+// \--- org.jetbrains.kotlin:kotlin-scripting-jvm-host:2.0.0
+// +--- org.jetbrains.kotlin:kotlin-script-runtime:2.0.0
+// +--- org.jetbrains.kotlin:kotlin-compiler-embeddable:2.0.0
+// |    +--- org.jetbrains.kotlin:kotlin-daemon-embeddable:2.0.0
+// |    \--- org.jetbrains.intellij.deps:trove4j:1.0.20200330
+// \--- org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:2.0.0
+// +--- org.jetbrains.kotlin:kotlin-scripting-compiler-impl-embeddable:2.0.0
 
 fun ExtraJavaModuleInfoPluginExtension.addModules() {
-    /*
-+--- org.jetbrains.kotlin:kotlin-scripting-common:1.8.21
-|    \--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21
-|         +--- org.jetbrains.kotlin:kotlin-stdlib-common:1.8.21
-|         \--- org.jetbrains:annotations:13.0
-     */
-    module("org.jetbrains.kotlin:kotlin-scripting-common", "kotlin.scripting.common")
-    // stdlib have well-defined explicit descriptors.
+    // +--- org.jetbrains.kotlin:kotlin-scripting-common:2.0.0
+    automaticModule("org.jetbrains.kotlin:kotlin-scripting-common", "kotlin.scripting.common")
+    // |    \--- org.jetbrains.kotlin:kotlin-stdlib:2.0.0
     knownModule("org.jetbrains.kotlin:kotlin-stdlib", "kotlin.stdlib")
-    // stdlib common doesn't have any descriptors nor dependencies.
-    module("org.jetbrains.kotlin:kotlin-stdlib-common", "kotlin.stdlib.common")
-    // jetbrains annotations doesn't have any descriptors.
-    module("org.jetbrains:annotations", "org.jetbrains.annotations")
-    /*
-+--- org.jetbrains.kotlin:kotlin-reflect:1.8.21
-|    \--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-     */
+    // |         \--- org.jetbrains:annotations:13.0
+    automaticModule("org.jetbrains:annotations", "org.jetbrains.annotations")
+    // +--- org.jetbrains.kotlin:kotlin-reflect:2.0.0
     knownModule("org.jetbrains.kotlin:kotlin-reflect", "kotlin.reflect")
-    /*
-+--- org.jetbrains.kotlin:kotlin-scripting-jvm:1.8.21
-|    +--- org.jetbrains.kotlin:kotlin-script-runtime:1.8.21
-|    +--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-|    \--- org.jetbrains.kotlin:kotlin-scripting-common:1.8.21 (*)
-     */
-    module("org.jetbrains.kotlin:kotlin-scripting-jvm", "kotlin.scripting.jvm")
-    // script runtime doesn't have any descriptors nor dependencies.
-    module("org.jetbrains.kotlin:kotlin-script-runtime", "kotlin.script.runtime")
-    /*
-\--- org.jetbrains.kotlin:kotlin-scripting-jvm-host:1.8.21
-     +--- org.jetbrains.kotlin:kotlin-script-runtime:1.8.21
-     +--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-     +--- org.jetbrains.kotlin:kotlin-scripting-common:1.8.21 (*)
-     +--- org.jetbrains.kotlin:kotlin-scripting-jvm:1.8.21 (*)
-     +--- org.jetbrains.kotlin:kotlin-compiler-embeddable:1.8.21
-     |    +--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-     |    +--- org.jetbrains.kotlin:kotlin-script-runtime:1.8.21
-     |    +--- org.jetbrains.kotlin:kotlin-reflect:1.6.10 -> 1.8.21 (*)
-     |    +--- org.jetbrains.kotlin:kotlin-daemon-embeddable:1.8.21
-     |    +--- org.jetbrains.intellij.deps:trove4j:1.0.20200330
-     |    \--- net.java.dev.jna:jna:5.6.0
-     \--- org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:1.8.21
-          +--- org.jetbrains.kotlin:kotlin-scripting-compiler-impl-embeddable:1.8.21
-          |    +--- org.jetbrains.kotlin:kotlin-scripting-common:1.8.21 (*)
-          |    +--- org.jetbrains.kotlin:kotlin-scripting-jvm:1.8.21 (*)
-          |    \--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-          \--- org.jetbrains.kotlin:kotlin-stdlib:1.8.21 (*)
-     */
-    // scripting jvm host doesn't have any dependencies.
-    module("org.jetbrains.kotlin:kotlin-scripting-jvm-host", "kotlin.scripting.jvm.host")
-
-    module("org.jetbrains.kotlin:kotlin-compiler-embeddable", "kotlin.compiler.embeddable")
-    module("org.jetbrains.kotlin:kotlin-daemon-embeddable", "kotlin.daemon.embeddable")
-    module("org.jetbrains.intellij.deps:trove4j", "trove4j")
-    module("net.java.dev.jna:jna", "com.sun.jna")
-
-    module("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable", "kotlin.scripting.compiler.embeddable")
-    module(
+    // +--- org.jetbrains.kotlin:kotlin-scripting-jvm:2.0.0
+    automaticModule("org.jetbrains.kotlin:kotlin-scripting-jvm", "kotlin.scripting.jvm")
+    // |    +--- org.jetbrains.kotlin:kotlin-script-runtime:2.0.0
+    automaticModule("org.jetbrains.kotlin:kotlin-script-runtime", "kotlin.script.runtime")
+    // \--- org.jetbrains.kotlin:kotlin-scripting-jvm-host:2.0.0
+    automaticModule("org.jetbrains.kotlin:kotlin-scripting-jvm-host", "kotlin.scripting.jvm.host")
+    // +--- org.jetbrains.kotlin:kotlin-compiler-embeddable:2.0.0
+    automaticModule("org.jetbrains.kotlin:kotlin-compiler-embeddable", "kotlin.compiler.embeddable")
+    // |    +--- org.jetbrains.kotlin:kotlin-daemon-embeddable:2.0.0
+    automaticModule("org.jetbrains.kotlin:kotlin-daemon-embeddable", "kotlin.daemon.embeddable")
+    // |    \--- org.jetbrains.intellij.deps:trove4j:1.0.20200330
+    automaticModule("org.jetbrains.intellij.deps:trove4j", "trove4j")
+    // \--- org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:2.0.0
+    automaticModule("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable", "kotlin.scripting.compiler.embeddable")
+    // +--- org.jetbrains.kotlin:kotlin-scripting-compiler-impl-embeddable:2.0.0
+    automaticModule(
         "org.jetbrains.kotlin:kotlin-scripting-compiler-impl-embeddable", "kotlin.scripting.compiler.impl.embeddable"
     )
 }
